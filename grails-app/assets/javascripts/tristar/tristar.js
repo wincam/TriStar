092d68c289a92e23e15810eae873c6a484864b06
Cameron Nicolle <cameron.nicolle@outlook.com>
2017-04-05 20:47:13 -0400

    Init

---
+++ grails-app/assets/javascripts/tristar/tristar.js
@@ -0,0 +1,9 @@
+//= wrapped
+//= require /angular/angular
+//= require /tristar/core/tristar.core
+//= require /tristar/index/tristar.index
+
+angular.module("tristar", [
+        "tristar.core",
+        "tristar.index"
+    ]);
---
+++ grails-app/init/tristar/BootStrap.groovy
@@ -0,0 +1,9 @@
+package tristar
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
