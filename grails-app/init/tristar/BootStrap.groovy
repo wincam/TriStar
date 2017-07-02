e59b0bc24c278cd0b67fe767e50368577c2027ab
Cameron Nicolle <cameron.nicolle@outlook.com>
2017-07-02 15:35:28 -0400

    Fixes javascript long compatibility

--- grails-app/init/tristar/BootStrap.groovy
+++ grails-app/init/tristar/BootStrap.groovy
@@ -14,8 +14,8 @@ class BootStrap {
             returnSet.username = it.username
             returnSet.teamMember = it.teamMember.collect {it.name}
             returnSet.teamCaptain = it.teamCaptain.collect {it.name}
-            returnSet.assignedTasks = it.assignedTasks.collect {it.getId()}
-            returnSet.createdTasks = it.createdTasks.collect {it.getId()}
+            returnSet.assignedTasks = it.assignedTasks.collect {Long.toString(it.getId())}
+            returnSet.createdTasks = it.createdTasks.collect {Long.toString(it.getId())}
             return returnSet
         }
 
@@ -25,13 +25,13 @@ class BootStrap {
             returnSet.description = it.description
             returnSet.captains = it.captains.collect {it.getUsername()}
             returnSet.members = it.members.collect {it.getUsername()}
-            returnSet.tasks = it.tasks.collect {it.getId()}
+            returnSet.tasks = it.tasks.collect {Long.toString(it.getId())}
             return returnSet
         }
 
         JSON.registerObjectMarshaller(Task) {
             def returnSet = [:]
-            returnSet.id = it.id
+            returnSet.id = Long.toString(it.id)
             returnSet.name = it.name
             returnSet.description = it.description
             returnSet.dueDate = it.dueDate
