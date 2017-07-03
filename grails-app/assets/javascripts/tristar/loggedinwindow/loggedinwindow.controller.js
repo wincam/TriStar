108129e2c406a3df45622982baa0264b542cedba
Cameron Nicolle <cameron.nicolle@outlook.com>
2017-07-02 23:44:18 -0400

    Fixes user listing

--- grails-app/assets/javascripts/tristar/loggedinwindow/loggedinwindow.controller.js
+++ grails-app/assets/javascripts/tristar/loggedinwindow/loggedinwindow.controller.js
@@ -11,7 +11,18 @@ function LoggedInWindowController(currentUser, userList, $stateParams, $state) {
     var ctrl = this;
     ctrl.teams = currentUser.teamMember.concat(currentUser.teamCaptain).sort();
     ctrl.userList = userList;
-    ctrl.userPage = $stateParams.pageId;
+    ctrl.userPage = $stateParams.userPageId;
+
+    // goes to the next user page
+    ctrl.userListNextPage = function () {
+        $state.go($state.current,{userPageId : (ctrl.userPage + 1)});
+    };
+
+    // goes to previous user page
+    ctrl.userListPreviousPage = function () {
+        console.log("Previous page");
+        $state.go($state.current,{userPageId : (ctrl.userPage - 1)});
+    };
 }
 
 
